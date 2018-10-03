01cd183b7e6daf8fa200cc193d95fc3ea6782c0b
Eloi Pardo <eloipr94@gmail.com>
2018-10-03 13:51:46 +0200

    changed db

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'queuemanager.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'queuemanager.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
