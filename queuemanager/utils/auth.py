4c9df4003931d6a1613f61264ad492c9c6be7642
Eloi Pardo <eloipr94@gmail.com>
2019-02-19 16:06:07 +0100

    handle invalid token

--- queuemanager/utils/auth.py
+++ queuemanager/utils/auth.py
@@ -1,3 +1,4 @@
+import jwt
 from functools import wraps
 from flask import request
 from queuemanager.db_manager import DBManager
@@ -10,7 +11,12 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get("Authorization")
-        user_id = User.decode_auth_token(token)
+        try:
+            user_id = User.decode_auth_token(token)
+        except jwt.ExpiredSignatureError:
+            return "Token has expired", 401
+        except jwt.InvalidTokenError:
+            return "Invalid token", 401
         if not token or not user_id:
             return "You have to login with proper credentials", 401
         job_id = kwargs.get("job_id")
@@ -26,7 +32,12 @@ def requires_admin(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = request.headers.get("Authorization")
-        user_id = User.decode_auth_token(token)
+        try:
+            user_id = User.decode_auth_token(token)
+        except jwt.ExpiredSignatureError:
+            return "Token has expired", 401
+        except jwt.InvalidTokenError:
+            return "Invalid token", 401
         if not token or not user_id:
             return "You have to login with proper credentials", 401
         user = db.get_user(user_id)
