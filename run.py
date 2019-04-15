3ffa6b18de6109f904b8123375c8d9466add8e7c
Marc Bermejo <marcbermejotarrago@gmail.com>
2019-04-15 11:11:39 +0200

    Added argument parsing to the run.py file

--- run.py
+++ run.py
@@ -10,10 +10,20 @@ __maintainer__ = "Marc Bermejo"
 __email__ = "mbermejo@bcn3dtechnologies.com"
 __status__ = "Development"
 
+import argparse
+
 from queuemanager import create_app
 
+parser = argparse.ArgumentParser(description='Run the queue manager server')
+parser.add_argument('--host', type=str, default='0.0.0.0',
+                    help='Address where the server is listening for connections')
+parser.add_argument('--port', type=int, default=5000,
+                    help='Port where the server is listening for connections')
+
+
 if __name__ == "__main__":
+    args = parser.parse_args()
     app = create_app(__name__, init_db_static_values=True)
 
     from queuemanager.socketio import socketio
-    socketio.run(app, debug=(app.config.get("DEBUG") > 0), host='0.0.0.0')
+    socketio.run(app, debug=(app.config.get("DEBUG") > 0), host=args.host, port=args.port)
