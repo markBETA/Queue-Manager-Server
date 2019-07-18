5a36c857ebec00bd93a973e7ff525855cc8d36cc
Marc Bermejo <marcbermejotarrago@gmail.com>
2019-07-18 12:33:00 +0200

    The data folder is no longer needed when building the database.

--- build_db.py
+++ build_db.py
@@ -13,8 +13,6 @@ __status__ = "Development"
 import os
 
 if __name__ == '__main__':
-    os.makedirs('./data', exist_ok=True)
-
     from flask import Flask
     app = Flask(__name__, instance_relative_config=True)
 
