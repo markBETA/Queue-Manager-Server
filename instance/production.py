eb5a3c03de0cde0a1387c03b844ce05ff3cee7ec
Marc Bermejo Tarragó <marcbermejotarrago@gmail.com>
2020-01-11 06:49:33 +0100

    Changed production settings

--- instance/production.py
+++ instance/production.py
@@ -12,11 +12,13 @@ class Config(_Config):
     ENV = "production"
 
     SQLALCHEMY_BINDS = {
-        'app': 'postgresql+psycopg2://postgres:bcnraprep@queue-manager-database.c8lme8jgnxxk.eu-west-3.rds.amazonaws.com/app',
+        'app': 'postgresql+psycopg2://postgres:7T#uZqxfL[z%,GGA@queue-manager-database.cdsfc1sk270d.eu-west-1.rds.amazonaws.com/app',
     }
 
-    FILE_MANAGER_UPLOAD_DIR = '/opt/deployment/files/'
+    FILE_MANAGER_UPLOAD_DIR = '/mnt/efs/queuemanager/shared-files/'
 
-    SOCKETIO_MESSAGE_QUEUE = "redis://queue-manager-cache.4mhe1r.ng.0001.euw3.cache.amazonaws.com:6379/1"
+    SOCKETIO_MESSAGE_QUEUE = "redis://queue-manager-redis.kklcm3.0001.euw1.cache.amazonaws.com:6379"
+
+    AUTHORIZATION_SUBREQUEST_URL = "http://172.31.29.210:80/"
 
     CORS_ALLOWED_ORIGINS = ["http://queuemanagerbcn3d.ml", "http://www.queuemanagerbcn3d.ml"]
